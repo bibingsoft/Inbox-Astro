f6bebd667bfa99e49ad13ad44465298acbc80350
bibingsoft <bibinjoseph@gsoftcomm.net>
2022-12-21 11:46:54 +0530

    css changes

--- src/components/team-inbox/common/AccordionCustom.jsx
+++ src/components/team-inbox/common/AccordionCustom.jsx
@@ -5,7 +5,7 @@ function AccordionCustom(props) {
   return (
       <div className="sidebar">
           <div className="accordion accordion-custom-right mt-2" id={props.id}>
-                      <div className="accordion-item acc-card bg-white border-0 rounded mb-3 p-2 fs-6 position-relative">
+                      <div className="accordion-item acc-card shadow-sm bg-white border-0 rounded mb-3 p-2 fs-6 position-relative">
                         <div className="accordion-header bg-white" id="headingUpdate">
                           <a className="accordion-button collapsed head d-flex align-items-center bg-white" type="button"
                             data-bs-toggle="collapse" data-bs-target={props.collapseTarget} aria-expanded="false"
