f6d6cb475701bb8f795a6038463664dd3d0fa902
bibingsoft <bibinjoseph@gsoftcomm.net>
2022-12-21 09:16:12 +0530

    custom css removal

--- src/components/team-inbox/team-inbox-chat/ChatCustomer.jsx
+++ src/components/team-inbox/team-inbox-chat/ChatCustomer.jsx
@@ -3,9 +3,9 @@ import React from "react";
 function chatLeft({ username, time, chatCustomer }) {
     return (
         <div className="chat-left">
-                                  <div className="d-flex flex-row chat-flex">
+                                  <div className="d-flex flex-row gap-2">
                                     <div className="user-ico-div rounded-circle">
-                                      <div className=" h-3 w-3 d-flex align-items-center justify-content-center rounded-circle bg-light-grey me-1">
+                                      <div className=" h-4 w-4 d-flex align-items-center justify-content-center rounded-circle bg-light-grey me-1">
                                         <svg width="12" height="11" viewBox="0 0 12 11" fill="none" xmlns="http://www.w3.org/2000/svg">
                                           <path d="M0.779298 3.99813C0.779022 3.58682 0.859885 3.1795 1.01725 2.79948C1.17462 2.41947 1.40541 2.07423 1.69639 1.78354C1.98738 1.49284 2.33285 1.2624 2.71302 1.10541C3.09319 0.948425 3.5006 0.86797 3.91191 0.868657H8.08872C9.81844 0.868657 11.2213 2.27572 11.2213 3.99813V10.2665H3.91191C2.18218 10.2665 0.779298 8.85942 0.779298 7.13701V3.99813ZM7.04452 5.04547V6.08967H8.08872V5.04547H7.04452ZM3.91191 5.04547V6.08967H4.95611V5.04547H3.91191Z" fill="#4B4A60"></path>
                                         </svg>
